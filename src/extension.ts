14abfdfff27f4add7021ea076e52191f3baad709
Daniil Kolesnichenko <garlicbreadcleric@yahoo.com>
2023-06-12 17:36:01 +0400

    v0.0.3: Add Markane executable path to settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,110 +1,164 @@
 import { spawn } from "child_process";
 
-import * as vscode from 'vscode';
-import { workspace, ExtensionContext } from 'vscode';
+import * as vscode from "vscode";
+import { workspace, ExtensionContext } from "vscode";
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions
-} from 'vscode-languageclient/node';
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 const clientId = "markane";
 
+function getMarkanePath(): string {
+  const config = vscode.workspace.getConfiguration("markane");
+  return config?.server?.path ?? "markane";
+}
+
 export async function activate(context: ExtensionContext) {
-	const restartCmd = vscode.commands.registerCommand(`${clientId}.restart`, async () => {
-		await stopClient();
-		startClient();
-	});
+  const restartCmd = vscode.commands.registerCommand(
+    `${clientId}.restart`,
+    async () => {
+      await stopClient();
+      startClient();
+    }
+  );
 
-	const logCmd = vscode.commands.registerCommand(`${clientId}.log`, async () => {
-		if (!client) return;
-		client.outputChannel.show(true);
-	});
+  const logCmd = vscode.commands.registerCommand(
+    `${clientId}.log`,
+    async () => {
+      if (!client) {
+        return;
+      }
+      client.outputChannel.show(true);
+    }
+  );
 
-	const indexCmd = vscode.commands.registerCommand(`${clientId}.index`, async () => {
-		if (!client) return;
-		client.sendNotification("markane/indexDocuments");
-	});
+  const indexCmd = vscode.commands.registerCommand(
+    `${clientId}.index`,
+    async () => {
+      if (!client) {
+        return;
+      }
+      client.sendNotification("markane/indexDocuments");
+    }
+  );
 
-	context.subscriptions.push(restartCmd);
-	context.subscriptions.push(logCmd);
-	context.subscriptions.push(indexCmd);
+  context.subscriptions.push(restartCmd);
+  context.subscriptions.push(logCmd);
+  context.subscriptions.push(indexCmd);
 
-  const supportedMarkaneVersions = new Set(["0.0.3", "0.0.4", "0.0.5", "0.0.6"]);
+  const supportedMarkaneVersions = new Set([
+    "0.0.3",
+    "0.0.4",
+    "0.0.5",
+    "0.0.6",
+  ]);
   const markaneVersion = await getMarkaneVersion();
   if (markaneVersion == null) {
-    vscode.window.showErrorMessage("Markane is not installed.");
+    vscode.window.showErrorMessage(
+      `Markane ${markaneVersion} is not installed.`
+    );
     return;
   }
 
   if (!supportedMarkaneVersions.has(markaneVersion)) {
-    vscode.window.showErrorMessage(`Markane version ${markaneVersion} is not supported.`);
+    vscode.window.showErrorMessage(
+      `Markane version ${markaneVersion} is not supported.`
+    );
     return;
   }
 
-	startClient();
+  startClient();
 }
 
 export async function deactivate(): Promise<void> {
-	await stopClient();
+  await stopClient();
 }
 
 async function getMarkaneVersion(): Promise<string | null> {
   const versionOutput: string | null = await new Promise((resolve, reject) => {
-    const child = spawn("markane", ["version"]);
+    const child = spawn(getMarkanePath(), ["version"]);
     let output = "";
-    child.stdout.on("data", (data) => { output += data; });
-    child.on("exit", (code) => { if (code === 0) { resolve(output); } else { resolve(null); } });
-    child.on("error", (err) => { resolve(null); });
+    child.stdout.on("data", (data) => {
+      output += data;
+    });
+    child.on("exit", (code) => {
+      if (code === 0) {
+        resolve(output);
+      } else {
+        resolve(null);
+      }
+    });
+    child.on("error", (err) => {
+      resolve(null);
+    });
   });
 
   return versionOutput?.trim() ?? null;
 }
 
 async function startClient() {
-	const serverOptions: ServerOptions = {
-		run: { command: "markane", args: ["lsp"] },
-		debug: { command: "markane", args: ["lsp"] }
-	};
+  const markanePath = getMarkanePath();
+  const serverOptions: ServerOptions = {
+    run: { command: markanePath, args: ["lsp"] },
+    debug: { command: markanePath, args: ["lsp"] },
+  };
 
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'markdown' }],
-		markdown: {
-			supportHtml: true
-		},
-		synchronize: {
-			fileEvents: workspace.createFileSystemWatcher('**/*.md'),
-		}
-	};
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "markdown" }],
+    markdown: {
+      supportHtml: true,
+    },
+    synchronize: {
+      fileEvents: workspace.createFileSystemWatcher("**/*.md"),
+    },
+  };
 
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
+  client = new LanguageClient(
+    "markane",
+    "Markane",
+    serverOptions,
+    clientOptions
+  );
 
-	await client.start();
+  await client.start();
 
-  client.onNotification("markane/showReferences", async ([uri, position, locations]) => {
-    await vscode.commands.executeCommand("editor.action.showReferences",
-      vscode.Uri.parse(uri),
-      new vscode.Position(position.line, position.character),
-      locations.map((l: any) => new vscode.Location(vscode.Uri.parse(l.uri), l.range)));
-  });
+  client.onNotification(
+    "markane/showReferences",
+    async ([uri, position, locations]) => {
+      await vscode.commands.executeCommand(
+        "editor.action.showReferences",
+        vscode.Uri.parse(uri),
+        new vscode.Position(position.line, position.character),
+        locations.map(
+          (l: any) => new vscode.Location(vscode.Uri.parse(l.uri), l.range)
+        )
+      );
+    }
+  );
 
-  client.onNotification("markane/showMentions", async ([uri, position, locations]) => {
-    await vscode.commands.executeCommand("editor.action.peekLocations",
-      vscode.Uri.parse(uri),
-      new vscode.Position(position.line, position.character),
-      locations.map((l: any) => new vscode.Location(vscode.Uri.parse(l.uri), l.range)));
-  });
+  client.onNotification(
+    "markane/showMentions",
+    async ([uri, position, locations]) => {
+      await vscode.commands.executeCommand(
+        "editor.action.peekLocations",
+        vscode.Uri.parse(uri),
+        new vscode.Position(position.line, position.character),
+        locations.map(
+          (l: any) => new vscode.Location(vscode.Uri.parse(l.uri), l.range)
+        )
+      );
+    }
+  );
 }
 
 async function stopClient() {
-	if (!client) return;
-	await client.stop();
-	client.outputChannel.dispose();
-	client.traceOutputChannel.dispose();
+  if (!client) {
+    return;
+  }
+  await client.stop();
+  client.outputChannel.dispose();
+  client.traceOutputChannel.dispose();
 }
